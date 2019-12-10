10d25ae1ec81764adae81daeab10ab95a4f3f7b2
Patrick Wendel <ptrckwndl13@gmail.com>
2019-12-10 14:30:31 -0700

    update practice code

--- 7kyu/BorrowSpeak.js
+++ 7kyu/BorrowSpeak.js
@@ -1,6 +1,20 @@
 //write function that removes punctuation and changes all text to lower case
 
 function borrow(s){
+    let string = s.toLowerCase()
+    let array = string.split('')
+    let correctArray = []
+    
+    for ( value of array ) {
+
+        if ( value !== ' ' && value !== '!' && value !== '?') {
+            
+            correctArray.push(value)
+            
+        }
+    }
+    return correctArray.join('')
     
 }
 
+console.log(borrow('WhAt! FiCK! DaMn CAke?'))
